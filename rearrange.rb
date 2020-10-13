d58ace5ce7f14433928c06071c8f4b08fc93508c
David Little <drabbytux@gmail.com>
2020-10-13 14:33:26 -0400

    Fixed negatives

--- rearrange.rb
+++ rearrange.rb
@@ -3,19 +3,25 @@
 # David Little
 # Oct 13 2020
 
-
 def getNum(message)
   print message
   num = gets.chomp
   if num =~ /\A\-?\d+\z/
-    #Removed .to_i - Leave it as a string.
     return num  
   end
 end
 
+
 def rearrange(num)
   negative = (num.delete! '-') ? true : false
-  arr_number = num.split('').sort_by { |n| n[/\d+/].to_i }.reverse
+  if( negative )
+    arr_number = num.split('').sort_by{|n| n[/\d+/].to_i}
+    arr_zeros = arr_number.select{|n| n == '0'}
+    arr_number.delete_if{|n| n == '0'}
+    arr_zeros.each {| n | arr_number.insert(1, '0')}
+  else
+    arr_number = num.split('').sort_by{|n| n[/\d+/].to_i}.reverse
+  end
   return negative ? '-' + arr_number.join('') : arr_number.join('');
 end
 
