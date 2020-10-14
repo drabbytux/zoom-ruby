f2f0e396c0796539e49b2eaaeec02cadb666e193
David Little <drabbytux@gmail.com>
2020-10-14 08:11:44 -0400

    Keep track of guesses

--- game.rb
+++ game.rb
@@ -8,3 +8,14 @@ print "What's your name? "
 input = gets
 name = input.chomp
 puts "Welcome, #{name}!"
+
+
+# Store a random number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
+
+# track how many guesses the player has made.
+num_guesses = 0
+
+puts "You've got #{10 - num_guesses} guesses left."
