da9ba2639e2cdafaaa48d78c377a6f7e1f78510e
David Little <drabbytux@gmail.com>
2020-10-14 11:16:04 -0400

    Fixed print/puts

--- happy_birthday_to_you.rb
+++ happy_birthday_to_you.rb
@@ -3,10 +3,10 @@
 # David Little
 # Oct 14 2020
 
-puts "What's your name?"
+print "What's your name?"
 name = gets.chomp
 
-puts "Happy birthday yo you"
+puts "Happy birthday to you"
 puts "Happy birthday to you"
 puts "Happy birthday dear #{name}"
 puts "Happy birthday to you."
