a5094584ea2ca0a79af3153adeadd4d49f0f299a
David Little <drabbytux@gmail.com>
2020-10-20 11:08:50 -0400

    Added the WRONG way method

--- price.rb
+++ price.rb
@@ -34,3 +34,29 @@ def show_discounts(prices)
 end
 
 show_discounts(prices)
+
+
+# New stuff 165
+def do_something_with_every_item(array, operation)
+
+  if operation == "total" or operation == "refund"
+    amount = 0
+  end
+  index = 0 
+
+  while index < array.length
+    if operation == "total"
+      amount += array[index]
+    elsif operation == "refund"
+      amount -= array[index]
+    elsif operation == "show discounts"
+      amount_off = array[index] / 3.0
+      puts format("Your discount: $%.2f", amount_off)
+    end
+    index += 1
+  end
+
+  if operation == "total" or operation == "refund"
+    return amount
+  end
+end
