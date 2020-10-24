0f90dbd193addc817e5fa276c34d111d24e96351
David Little <drabbytux@gmail.com>
2020-10-24 09:58:33 -0400

    Rviews q 10

--- reviews.rb
+++ reviews.rb
@@ -13,8 +13,26 @@ def reject
   end
 end
 
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
 relevant_lines = lines.find_all{|line|line.include?("Truncated")}
 puts relevant_lines
 
 reviews = relevant_lines.reject{|line| line.include?("--")}
 p reviews
+
+string = reviews.first
+words = string.split(" ")
+p words
+
+adjectives = []
+
+reviews.each do |review|
+  adjectives << find_adjective(review)
+end
+
+puts adjectives
