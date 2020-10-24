18d6ef418d08e28a7e14c67341072919f6488708
David Little <drabbytux@gmail.com>
2020-10-24 09:53:47 -0400

    Reviews Q 9

--- reviews.rb
+++ reviews.rb
@@ -3,6 +3,18 @@ File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
-relevant_lines = lines.find_all{|line|line.include?("Truncated")}
+def reject
+  kept_items = []
+  self.each do |item|
+    unless yield(item)
+      kept_items << itemend
+    end
+    kept_items
+  end
+end
 
+relevant_lines = lines.find_all{|line|line.include?("Truncated")}
 puts relevant_lines
+
+reviews = relevant_lines.reject{|line| line.include?("--")}
+p reviews
