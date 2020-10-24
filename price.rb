345263c3a3603b6afd305c7328f96e17c4e9e9fa
David Little <drabbytux@gmail.com>
2020-10-24 08:33:01 -0400

    More Prices question 7

--- price.rb
+++ price.rb
@@ -6,9 +6,6 @@ def total(prices)
   amount
 end
 
-prices = [3.99, 25.00, 8.99]
-
-puts format("%.2f", total(prices))
 
 def refund(prices)
   amount = 0
@@ -18,15 +15,17 @@ def refund(prices)
   amount
 end
 
-puts format("%.2f", refund(prices))
+
 
 def show_discounts(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
+  prices.each do |price|
+    amount_off = price / 3.0
     puts format("your discount: $%.2f", amount_off)
-    index += 1
   end
 end
 
+prices = [3.99, 25.00, 8.99]
+
+puts format("%.2f", total(prices))
+puts format("%.2f", refund(prices))
 show_discounts(prices)
