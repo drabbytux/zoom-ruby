8063d11a41f4e4619ee8606d4fd7fa74100a9eb5
David Little <drabbytux@gmail.com>
2020-10-26 11:43:03 -0400

    Vehicles Question 5

--- vehicles.rb
+++ vehicles.rb
@@ -1,48 +1,4 @@
-class Car
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-  def accelerate
-    puts "Floor it!"
-  end
-
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn front 2 wheels."
-  end
-
-end
-
-
-class Truck
-  attr_accessor :odometer
-  attr_accessor :gas_used
-
-  def mileage
-    @odometer / @gas_used
-  end
-  def accelerate
-    puts "Floor it!"
-  end
-
-  def sound_horn
-    puts "Beep! Beep!"
-  end
-
-  def steer
-    puts "Turn front 2 wheels."
-  end
-
-end
-
-
-class Motorcycle
+class Vehicle
   attr_accessor :odometer
   attr_accessor :gas_used
 
