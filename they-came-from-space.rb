d345bd457cdb9db46fde480794fd40ee317cd486
David Little <drabbytux@gmail.com>
2020-10-27 17:43:18 -0400

    They came from space, Part 2

--- they-came-from-space.rb
+++ they-came-from-space.rb
@@ -1,7 +1,6 @@
 
 # - - - - - - - Organism base CLASS - - - - - - - #
 class Organism
-
   attr_accessor :name, :hunger_level, :health_level, :energy_level
   attr_accessor :num_of_arms, :num_of_legs, :num_of_heads, :lifespan_in_years, :weight, :num_of_eyes, :general_color
 
@@ -40,6 +39,7 @@ class Organism
 end
 
 
+
 # - - - - - - - Distinct organism CLASSES - - - - - - - #
 class Flaneclop < Organism
   def initialize name
@@ -72,7 +72,7 @@ class PloopyPloop < Organism
     super
     @num_of_arms = 2
     @num_of_legs = 2
-    @num_of_heads = 4
+    @num_of_heads = 0
     @lifespan_in_years = 65
     @weight = 245
     @num_of_eyes = 2
@@ -121,15 +121,34 @@ class SpaceShip
     organism.each {|o| @passengers << o }
   end
 
+  def disembark
+    puts "All passengers have disembarked"
+    pass = @passengers.map {|p| p }
+    @passengers.clear
+    pass
+  end
+
   def blast_off
     # initial fuel use
+    puts "PPppppppchchcchchchchchcssssssssshhhhhhshshssh! Blast off!!!"
     self.consume_fuel
   end
 
   def consume_fuel
-    puts "PPppppppchchcchchchchchcssssssssshhhhhhshshssh! Blast off!!!"
     @fuel_amount = @fuel_amount - Light_year_fuel_usage_amount
     puts "Fuel level remaing: #{fuel_amount} gigawatts"
+  end
+
+  def travel_to planet
+    planet.light_years_from_earth.ceil().times do
+      sleep 1
+      self.consume_fuel
+      if @fuel_amount <= 0
+        puts "The ship ran out of fuel.. :("
+        return false
+      end
+    end
+    true
   end
 
   def who_is_on_board
@@ -140,6 +159,37 @@ class SpaceShip
 
 end
 
+# - - - - - - - Planet CLASS - - - - - - - #
+class Planet
+  attr_accessor :name, :number_of_moons, :distance_from_sun, :atmosphere_composition, :light_years_from_earth
+  attr_accessor :inhabitants
+
+  def initialize name
+    @name = name
+    @inhabitants = []
+  end
+
+  def see_the_future(years = 1)
+    # how many organisms are left
+    # any survive?
+    future_inhabitants = (@inhabitants.size ** 3 ) * years
+    puts "There are now #{future_inhabitants} inhabitants in #{years} years time."
+  end
+
+  def accept(visitors)
+    visitors.each {|v| @inhabitants << v }
+    puts "New visitors accepted"
+  end
+
+  def who_inhabits_this_place
+    print "Current Inhabitants: "
+      @inhabitants.each {|p| print "#{p.name} "}
+    print "\n"
+  end
+
+end
+
+
 
 # _________________________________________ #
 
@@ -149,6 +199,8 @@ o3 = PloopyPloop.new("*%sdk##askjk Jr.")
 o4 = Bombween.new("Sally")
 o5 = Platteemooz.new("R8")
 ship = SpaceShip.new
+kepler_1649c = Planet.new("Kepler-1649c")
+kepler_1649c.light_years_from_earth = 4.3
 
 o1.info
 o1.eat
@@ -156,6 +208,20 @@ o1.fight
 o1.fight
 o1.info
 
-ship.board(o1,o2,o3)
-ship.blast_off
+# All aboard!
+ship.board(o1,o2,o3,o4,o5)
 ship.who_is_on_board
+
+# Take off eh?
+ship.blast_off
+if( ship.travel_to( kepler_1649c ) )
+  puts "Yippeee! We landed on #{kepler_1649c.name}!!!"
+  puts "Everybody off!"
+  kepler_1649c.accept( ship.disembark )
+  puts kepler_1649c.who_inhabits_this_place
+
+  
+end
+
+# Test how many folks are there now! (years)
+kepler_1649c.see_the_future(12)
