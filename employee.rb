6cbdab9f2c36052fab26371d11d13d69cec42df3
David Little <drabbytux@gmail.com>
2020-10-28 09:57:22 -0400

    Emplyee Page 140

--- employee.rb
+++ employee.rb
@@ -15,6 +15,11 @@ end
 class SalariedEmployee < Employee
   attr_reader :salary
 
+  def initialize (name = "Anonymous", salary = 0.0)
+    self.name = name
+    self.salary = salary
+  end
+
   def salary=(salary)
     # Code to validate
     if salary >= 0
@@ -35,6 +40,12 @@ end
 class HourlyEmplyee < Employee
 
   attr_reader :hourly_wage, :hours_per_week
+
+  def initialize (name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    self.name = name
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
 
   def hourly_wage=(hourly_wage)
     if hourly_wage >= 0
@@ -63,14 +74,8 @@ end
 
 # ________________________________ #
 
-salaried_employee = SalariedEmployee.new
-salaried_employee.name = "Jane Doe"
-salaried_employee.salary = 50000
+salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
 salaried_employee.print_pay_stub
 
-
-hourly_employee = HourlyEmplyee.new
-hourly_employee.name = "John Smith"
-hourly_employee.hourly_wage = 14.97
-hourly_employee.hours_per_week = 30
+hourly_employee = HourlyEmplyee.new("John Smith", 14.97, 30)
 hourly_employee.print_pay_stub
