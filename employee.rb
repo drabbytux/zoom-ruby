b2f071e3728f2d0efe0c6fac2777cd4665ecfafe
David Little <drabbytux@gmail.com>
2020-10-28 10:18:19 -0400

    Employee Page 147-148

--- employee.rb
+++ employee.rb
@@ -73,6 +73,11 @@ class HourlyEmplyee < Employee
     formatted_pay = format("$%.2f", pay_for_period)
     puts "Pay This Period: #{formatted_pay}"
   end
+
+  def turn_into_cashier
+      self.hourly_wage = 12.75
+      self.hours_per_week = 25
+  end
 end
 
 
@@ -83,3 +88,17 @@ salaried_employee.print_pay_stub
 
 hourly_employee = HourlyEmplyee.new("John Smith", 14.97, 30)
 hourly_employee.print_pay_stub
+
+ivan = HourlyEmplyee.new("Ivan Stokes")
+ivan.turn_into_cashier
+ivan.print_pay_stub
+
+harold= HourlyEmplyee.new("Harold Nguyen", 12.75, 25)
+tamara = HourlyEmplyee.new("Tamara Wells", 12.75, 25)
+susie = HourlyEmplyee.new("Susie Powell", 12.75, 25)
+
+edwin = HourlyEmplyee.new("Edwin Burgess", 10.50, 20)
+ethel = HourlyEmplyee.new("Ethel Harris", 10.50, 20)
+
+angela = HourlyEmplyee.new("Angela Matthews", 19.25, 30)
+stewart = HourlyEmplyee.new("Stewart Sanchez", 19.25, 30)
