43be83ebd0065e939f743de25884fded40d4f744
David Little <drabbytux@gmail.com>
2020-10-28 09:38:10 -0400

    Emplyees Question 4

---
+++ employee.rb
@@ -0,0 +1,34 @@
+class Employee
+
+  attr_reader :name
+
+  def name=(name)
+    # code to validate
+    @name = name
+  end
+
+  def print_name
+    puts "Name: #{name}"
+  end
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  def salary=(salary)
+    # Code to validate
+    @salary = salary
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+end
+
+salaried_employee = SalariedEmployee.new
+salaried_employee.name = "Jane Doe"
+salaried_employee.salary = 50000
+salaried_employee.print_pay_stub
