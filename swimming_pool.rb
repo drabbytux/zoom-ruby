0cb2c538b51cee322e33b616b12c7b10d257bf60
David Little <drabbytux@gmail.com>
2020-10-30 11:53:18 -0400

    contructed swimming pool and bucket and coffee cup

---
+++ swimming_pool.rb
@@ -0,0 +1,10 @@
+#Exist in their own .rb file
+#Have at least 1 additional attribute
+#Have at least 1 additional method.
+#Overwrite at least 1 parent method.
+
+require_relative "vessel"
+
+class SwimmingPool < Vessel
+
+end
