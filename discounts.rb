6af200aade56a2bf9193575123789261b3f39702
David Little <drabbytux@gmail.com>
2020-11-26 10:39:56 -0500

    Adjusted the sum, the price amuonts and cleaned up code.

--- discounts.rb
+++ discounts.rb
@@ -1,4 +1,4 @@
-class Checkout
+class Cart
   attr_accessor :bill
 
   def initialize(order, prices, discounts)
@@ -6,16 +6,14 @@ class Checkout
     @prices = prices
     @discounts = discounts
     @bill = Hash.new
-    calculate_bill
+    calculate_total
   end
 
-  def calculate_bill
+  def calculate_total
     @order.each do |line_item_key, line_item_count|
-      if @discounts.has_key?(line_item_key)
-        @bill[line_item_key] = apply_discount(line_item_key, line_item_count, @discounts[line_item_key])
-      else
-        @bill[line_item_key] = line_item_count * @prices[line_item_key]
-      end
+      @bill[line_item_key] = @discounts.has_key?(line_item_key) ?
+        apply_discount(line_item_key, line_item_count, @discounts[line_item_key]) :
+        line_item_count * @prices[line_item_key]
     end
   end
 
@@ -24,11 +22,10 @@ class Checkout
   end
 
   def whats_the_damage?
-    tally=0
-    @bill.each {|key, val| tally = tally+val}
-    tally
+    @bill.reduce(0.0) {|sum, num| sum + num[1]}
   end
 end
+
 
 # Proc DISCOUNT definitions
 discount_type_two_for_one = Proc.new { |price, item_total|
@@ -38,20 +35,15 @@ discount_type_two_or_more_20_percent_off = Proc.new { |price, item_total|
   item_total >= 2 ? (price * item_total) - ((price * item_total) * 0.2) : price * item_total
 }
 
+# Associate products with discounts
 shop_discounts = {'grapes' => discount_type_two_for_one,'apples' => discount_type_two_or_more_20_percent_off }
-prices = { 'grapes'=> 5, 'apples'=> 4, 'peaches'=> 7}
 
-#  _ _ _ _ _ _ _ _ _ _ _ _#
+# Shop Prices
+prices = { 'grapes'=> 5, 'apples'=> 3, 'peaches'=> 7}
 
-order = [['grapes', 1], ['apples', 1], ['peaches',1]]
-c = Checkout.new(order, prices, shop_discounts)
+
+#  _______ TIME TO RUN _______#
+
+order = [['peaches', 7], ['grapes', 7], ['apples', 7]]
+c = Cart.new(order, prices, shop_discounts)
 puts c.whats_the_damage?
-
-=begin
- # eg. in ruby
- [ ['grapes', 1], ['apples', 0], ['peaches', 1] ] # output => 12
- [ ['grapes', 1], ['apples', 1], ['peaches', 1] ] # output => 15
- [ ['grapes', 2], ['apples', 2], ['peaches', 1] ] # output => 16.8
- [ ['grapes', 3], ['apples', 5], ['peaches', 2] ] # output => 36
- [ ['peaches', 7], ['grapes', 7], ['apples', 7] ] # output => 85.8
-=end
